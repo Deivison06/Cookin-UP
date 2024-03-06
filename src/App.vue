b2f1402a0f1f5f7f212488aae04c8fdc3a756483
Deivison Santos <deivison.gomes@avanz.com.br>
2024-03-06 15:09:28 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+    import Banner from './components/Banner.vue';
+    import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+    import Rodape from './components/Rodape.vue';
+
+    export default {
+      components: {
+        Banner: Banner,
+        ConteudoPrincipal: ConteudoPrincipal,
+        Rodape: Rodape,
+      }
+    }
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+  <Rodape />
+</template>
+<style scoped>
+</style>./components/conteudoPricipal.vue
